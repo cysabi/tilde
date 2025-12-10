fc105102c7524ec9ad088da295b44f68d83f39bc
cysabi <47790183+cysabi@users.noreply.github.com>
2025-12-09 19:38:39 -0500

    Enhance init.sh for Arch Linux installation
    Added installation steps for Arch Linux and user setup.

---
+++ .gists/init.sh
@@ -0,0 +1,13 @@
+# install arch
+wsl.exe --install archlinux
+
+# inside arch as root -- install packages
+pacman -Syu base base-devel git fish vi helix
+useradd -m -G wheel -s fish cysabi
+visudo # uncomment %wheel ALL=(ALL:ALL) NOPASSWD: ALL
+
+wsl.exe --manage archlinux --set-default-user cysabi
+
+# inside arch as cysabi -- run tilde
+curl https://raw.githubusercontent.com/cysabi/tilde/refs/heads/main/.config/fish/functions/tilde.fish | source
+tilde
